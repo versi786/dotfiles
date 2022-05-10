3552cf66f44639c49327b19ba57fe651241513ba
Aasif Versi <versi786@gmail.com>
2022-05-10 19:58:57 -0400

    fix reboot script

--- dotfiles/.bin/reboot_to_windows.sh
+++ dotfiles/.bin/reboot_to_windows.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo grub-reboot "$(grep -i windows /boot/grub/grub.cfg|cut -d"'" -f2)" && sudo reboot
+sudo grub-reboot "$(grep -i windows /boot/grub/grub.cfg | cut -d"'" -f2 | head -n 1)" && sudo reboot
