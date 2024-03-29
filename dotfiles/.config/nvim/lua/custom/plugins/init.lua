cf4b17b22b32b445f98fc696cf82ef38e867cff8
Aasif Versi <versi786@gmail.com>
2024-03-28 21:37:13 -0400

    moar plugins

--- dotfiles/.config/nvim/lua/custom/plugins/init.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/init.lua
@@ -1,7 +1,9 @@
 return {
 	{ "airblade/vim-gitgutter" },
 	{ "editorconfig/editorconfig-vim" },
+	{ "editorconfig/editorconfig-vim" },
 	{ "morhetz/gruvbox" },
 	{ "numToStr/Comment.nvim" },
+	{ "tpope/vim-sleuth" },
 	{ "vim-scripts/a.vim" },
 }
