4b57537fd2195dd54a2441af89a610fccdc94148
Aasif Versi <versi786@gmail.com>
2024-07-28 19:17:41 -0400

    updates

--- dotfiles/.config/nvim/lua/custom/plugins/nvim_lsp_config.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/nvim_lsp_config.lua
@@ -152,6 +152,7 @@ return { -- LSP Configuration & Plugins
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format lua code
+			"rust-analyzer",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
